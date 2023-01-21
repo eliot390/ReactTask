ae20ca0506e413b43cd21c628649f8d593a4abbd
eliot390 <eliot390@github.com>
2023-01-21 15:39:19 -0800

    added json-server package/abilities, css updates

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doc Appointment',
-      day: '1/12/23 @ 12:00pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Meeting',
-      day: '2/07/23 @ 10:00am',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Shopping',
-      day: '2/10/23 @ 2:00pm',
-      reminder: false,
-    },
-  ])
+  const [tasks, setTasks] = useState([])
+  
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  const fetchTasks = async () => {
+    const response = await fetch('http://localhost:5000/tasks')
+    const data = await response.json()
+
+    return data
+  }
 
   const addTask = (task) => {
     const id = Math.floor(Math.random()*100)
